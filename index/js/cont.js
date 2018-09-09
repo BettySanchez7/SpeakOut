a0f23308b90f916e1acea2ce75b57e44a3c2e406
BettySanchez7 <31338064+BettySanchez7@users.noreply.github.com>
2018-09-09 10:42:35 -0500

    Primer Commit
    Subiendo proyecto

---
+++ index/js/cont.js
@@ -0,0 +1,22 @@
+<script language="JavaScript"> 
+function Login(){ 
+var done=0; 
+var usuario=document.login.usuario.value; 
+var password=document.login.password.value; 
+if (usuario=="USUARIO1" && password=="CONTRASEÑA1") { 
+window.location=""; 
+} 
+if (usuario=="USUARIO2" && password=="CONTRASEÑA2") { 
+window.location=""; 
+} 
+if (usuario=="" && password=="") { 
+window.location="errorpopup.html"; 
+} 
+} 
+        </script> 
+</center> 
+<script language="Javascript"> 
+<!-- Begin 
+document.oncontextmenu = function(){return false} 
+// End --> 
+</script>
